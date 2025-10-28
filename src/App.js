266b25c5f4aab510966cbbad7affbbc9764e7d08
John Horo <johnhoro@Johns-MBP.alphionsee.in>
2025-10-28 14:18:29 +0530

    clean App.js file and move code in home.js file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React from 'react';
-import {
-  Header,
-  Navigation,
-  FloatingVideo,
-  ConsolidatedSections,
-  PinnedSection,
-  Footer
-} from './components';
-import { useNavigation, useScrollAnimations } from './hooks';
+import { Home } from './components';
 
 function App() {
-  const { activeSection, setActiveSection, handleSectionClick } = useNavigation();
-  const { videoRef } = useScrollAnimations(activeSection, setActiveSection);
-
-  return (
-    <div className="app-container">
-      {/* Header - Visible in sections 1, 2, 3, 4 but not footer */}
-      <Header />
-
-      {/* Navigation Dots */}
-      <Navigation
-        activeSection={activeSection}
-        onSectionClick={handleSectionClick}
-      />
-
-      {/* Floating Video */}
-      <FloatingVideo ref={videoRef} />
-
-      {/* Consolidated Sections 1, 2, 3 */}
-      <ConsolidatedSections />
-
-      {/* Section 4 - Pinned Section */}
-      <PinnedSection />
-
-      {/* Footer Section - Header hidden here */}
-      <Footer />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
